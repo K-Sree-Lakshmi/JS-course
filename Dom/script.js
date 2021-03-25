1e9d2f354cc1b9a4f588a35f9b4ae347a97fa79b
SreeLakshmiK23 <sree.lakshmi@nineleaps.com>
2021-03-25 13:40:34 +0530

    [script] refactor Tabbed component

--- Dom/script.js
+++ Dom/script.js
@@ -1,12 +1,16 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+///////////////////////////////////////
+// Modal window
+
 
 const openModal = function (e) {
   e.preventDefault();
@@ -19,7 +23,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-btnsOpenModal.forEach(btn=>btn.addEventListener('click',openModal))
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal))
 
 // for (let i = 0; i < btnsOpenModal.length; i++)
 //   btnsOpenModal[i].addEventListener('click', openModal);
@@ -46,50 +50,94 @@ header.append(message);
 // header.after(message)
 
 // deleting
-document.querySelector('.btn--close-cookie').addEventListener('click',function(){
+document.querySelector('.btn--close-cookie').addEventListener('click', function () {
   message.remove();
 })
 
 // styles
 
-message.style.background='#37383d';
-message.style.width='120%';
+message.style.background = '#37383d';
+message.style.width = '120%';
 
-message.style.height = Number.parseFloat(getComputedStyle(message).height,10)+30+'px';
+message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
 
-btnScrollTo.addEventListener('click',function(){
+
+btnScrollTo.addEventListener('click', function () {
   const s1cords = section1.getBoundingClientRect();
   console.log(s1cords);
   // window.scrollTo(s1cords.left+window.pageXOffset,s1cords.top+window.pageYOffset)
-  window.scrollTo({ left:s1cords.left+window.pageXOffset, top:s1cords.top+window.pageYOffset, behavior:'smooth'})
+  window.scrollTo({ left: s1cords.left + window.pageXOffset, top: s1cords.top + window.pageYOffset, behavior: 'smooth' })
 
 })
 
+// page navigation
 
-const randomInt = (min,max) => Math.floor(Math.random()*(max-min+1)+min);
+// Rather than simply attaching the same event handler to multiple elements we do event delation as shown below
+// document.querySelectorAll('.nav__link').forEach(function(el){
+//   el.addEventListener('click', function(e){
+//     e.preventDefault();
+//   const id = this.getAttribute('href')
+//   console.log(id)
+//   document.querySelector(id).scrollIntoView({behavior:'smooth'})
 
-const randomColor = () => `rgb(${randomInt(0,225)},${randomInt(0,225)},${randomInt(0,225)})`;
+// })
+// })
 
-console.log(randomColor(0,225))
-
-document.querySelector('.nav__link').addEventListener('click',function(e){
-  this.style.backgroundColor = randomColor()
-  console.log('LINK',e.target,e.currentTarget)
-  // stop propagation
-  // e.stopPropagation()
+// event delegation
+// 1.Add event listener to a common parent that we are interested in
+// 2. Determine what element originated the event
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  e.preventDefault();
+  // matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href')
+    console.log(id)
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' })
+  }
 })
 
-document.querySelector('.nav__links').addEventListener('click',function(e){
-  this.style.backgroundColor = randomColor()
-  console.log('CONTAINER',e.target,e.currentTarget)
+// Tabbed components
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  console.log(clicked)
+  if (!clicked) return;
+  // remove activation
+  tabs.forEach(t => t.classList.remove('operations__tab--active'))
+  tabsContent.forEach(t => t.classList.remove('operations__content--active'))
+// activate tabs
+  clicked.classList.add('operations__tab--active')
+
+  // activate content area
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
 })
 
-document.querySelector('.nav').addEventListener('click',function(e){
-  this.style.backgroundColor = randomColor()
-  console.log('NAV',e.target,e.currentTarget)
 
-},false)
+// const randomInt = (min,max) => Math.floor(Math.random()*(max-min+1)+min);
+
+// const randomColor = () => `rgb(${randomInt(0,225)},${randomInt(0,225)},${randomInt(0,225)})`;
+
+// console.log(randomColor(0,225))
+
+// document.querySelector('.nav__link').addEventListener('click',function(e){
+//   this.style.backgroundColor = randomColor()
+//   console.log('LINK',e.target,e.currentTarget)
+//   // stop propagation
+//   // e.stopPropagation()
+// })
+
+// document.querySelector('.nav__links').addEventListener('click',function(e){
+//   this.style.backgroundColor = randomColor()
+//   console.log('CONTAINER',e.target,e.currentTarget)
+
+// })
+
+// document.querySelector('.nav').addEventListener('click',function(e){
+//   this.style.backgroundColor = randomColor()
+//   console.log('NAV',e.target,e.currentTarget)
+
+// },false)
