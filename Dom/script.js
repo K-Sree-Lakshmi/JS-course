2cdad7cbb9ce93c200f6d8cacad782b4c35a0323
SreeLakshmiK23 <sree.lakshmi@nineleaps.com>
2021-04-06 12:22:39 +0530

    [script] refactor lazy loading images

--- Dom/script.js
+++ Dom/script.js
@@ -196,6 +196,7 @@ const stickyNav = function(entries){
   // destructure syntax
   const [entry] =  entries;
   console.log(entry)
+
   if(!entry.isIntersecting) nav.classList.add('sticky')
   else nav.classList.remove('sticky')
 }
@@ -206,3 +207,45 @@ const headerObserver = new IntersectionObserver(stickyNav,{
 })
 
 headerObserver.observe(headera)
+
+const sectionAll = document.querySelectorAll('.section')
+const revealSection =  function(entries, observer){
+  const [entry] = entries
+  console.log(entry)
+  if(!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden')
+  observer.unobserve(entry.target)
+}
+
+const sectionObserver = new IntersectionObserver(revealSection,{
+  root:null,
+  threshold: 0.15
+})
+sectionAll.forEach(function(section){
+  sectionObserver.observe(section)
+  section.classList.add('section--hidden')
+})
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]')
+
+const loadImg =  function(entries, observer){
+  const [entry] = entries
+  console.log(entry)
+  if(!entry.isIntersecting) return;
+// Replace src with data-src
+  entry.target.src = entry.target.dataset.src
+
+  entry.target.addEventListener('load', function(){
+    entry.target.classList.remove('lazy-img')
+  })
+  observer.unobserve(entry.target)
+}
+
+const imgObserver = new IntersectionObserver(loadImg,{
+  root:null,
+  threshold: 0,
+  rootMargin: '200px'
+})
+
+imgTargets.forEach(img => imgObserver.observe(img))
